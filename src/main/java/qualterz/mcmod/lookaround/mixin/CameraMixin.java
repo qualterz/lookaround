42e9138add4b9c4daa932434065f47cb16423b15
qualterz <qualterz@tutamail.com>
2022-02-07 13:26:17 +0200

    Modify rotation arguments

--- src/main/java/qualterz/mcmod/lookaround/mixin/CameraMixin.java
+++ src/main/java/qualterz/mcmod/lookaround/mixin/CameraMixin.java
@@ -1,27 +1,18 @@
 package qualterz.mcmod.lookaround.mixin;
 
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import qualterz.mcmod.lookaround.CameraManager;
 import net.minecraft.client.render.Camera;
-import net.minecraft.entity.Entity;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Camera.class)
 public abstract class CameraMixin
 {
-	@Inject(method = "update", at = @At("HEAD"))
-	private void onCameraUpdateBegin(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci)
+	@ModifyArgs(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;setRotation(FF)V"))
+	private void modifyRotationArgs(Args args)
 	{
-        focusedEntity.setYaw(CameraManager.lookYaw);
-        focusedEntity.setPitch(CameraManager.lookPitch);
-	}
-
-	@Inject(method = "update", at = @At("RETURN"))
-	private void onCameraUpdateEnd(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci)
-	{
-		focusedEntity.setYaw(CameraManager.actualYaw);
-		focusedEntity.setPitch(CameraManager.actualPitch);
+		args.set(0, CameraManager.lookYaw);
+		args.set(1, CameraManager.lookPitch);
 	}
 }
