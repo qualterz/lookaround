9199383a831ec9bef4c54355b6125de2b73bc9a8
qualterz <qualterz@tutamail.com>
2022-02-17 20:58:56 +0200

    Remove unnecessary fields

--- src/main/java/qualterz/minecraft/lookaround/LookAroundMod.java
+++ src/main/java/qualterz/minecraft/lookaround/LookAroundMod.java
@@ -3,18 +3,13 @@ package qualterz.minecraft.lookaround;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LookAroundMod implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("LookAround");
-
-	public static double offsetCrosshairX;
-	public static double offsetCrosshairY;
 
 	public static float actualYaw;
 	public static float actualPitch;
@@ -26,7 +21,6 @@ public class LookAroundMod implements ClientModInitializer {
 
 	public static boolean isDirectionLocked;
 	public static boolean shouldLockDirection;
-	public static boolean shouldDrawCrosshair;
 
 	@Override
 	public void onInitializeClient() {
@@ -42,7 +36,5 @@ public class LookAroundMod implements ClientModInitializer {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			shouldLockDirection = lookAroundBinding.isPressed();
 		});
-
-		shouldDrawCrosshair = true;
 	}
 }
